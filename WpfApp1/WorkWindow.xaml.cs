0b5edc34d91154ee947a788051edac8324a71c95
Родион Коробкин <@DESKTOP-86N34QN>
2024-03-18 15:32:40 +0300

    Версия 1

--- WpfApp1/WorkWindow.xaml.cs
+++ WpfApp1/WorkWindow.xaml.cs
@@ -17,9 +17,9 @@ namespace WpfApp1
     /// <summary>
     /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class WorkWindow : Window
     {
-        public Window1()
+        public WorkWindow()
         {
             InitializeComponent();
         }
